5f9b0c310614d0b3de698bd00b56fc437fc3572e
Rodrigo Farinango <rodrigofarii@gmail.com>
2023-04-14 12:37:37 -0600

    wip

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = ()=> {
                     <Logo />
                 </LogoContainer>
                 <PokeballContainer>
-                    <Pokeball />
+                    <Pokeball collectionSize={20}/>
                 </PokeballContainer>
             </NavContainer>
         </>
--- src/components/navbar/Pokeball.jsx
+++ src/components/navbar/Pokeball.jsx
@@ -1,10 +1,37 @@
-import logo from '../../pokeball.png'
+import React from 'react';
+import styled from 'styled-components';
+import pokeball from '../../pokeball.png';
+
+const PokeballIcon = styled.img`
+  width: 40px;
+`;
+
+const CollectionSize = styled.span`
+  position: absolute;
+  bottom: -5px;
+  left: -5px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10px;
+  color: white;
+  background-color: navy;
+  border-radius: 50%;
+  padding: 0;
+  margin: 0;
+`;
 
 
-const Pokeball = () => {
-    return(
-            <img src={logo} className="App-logo" alt='logo' width='40px'/>
-    );
-}
+const Pokeball = ({ collectionSize }) => {
+  return (
+    <>
+        <PokeballIcon src={pokeball} alt="pokeball icon" />
+        <CollectionSize>{collectionSize}</CollectionSize>
+    </>
+      
+  );
+};
 
 export default Pokeball;
